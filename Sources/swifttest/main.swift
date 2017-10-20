95fee482d6b0327a7bd706c0451b2169df512651
Daniel Pater <daniel.pater@appetise.me>
2017-10-20 21:17:07 +0200

    Initial commit.

---
+++ Sources/swifttest/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  swifttestPackageDescription
+//
+//  Created by Daniel Pater on 20.10.17.
+//
+
+import Foundation
+import MongoKitten
+
+
+let mongoDb = try! Database("mongodb://user:pass@host:port/databasename")
+
+while true { }
